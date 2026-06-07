ccbb43e7c4c2337ba29205c58190af92c4b32226
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-06-07 11:26:27 +0530

    Remove server folder

--- server/src/config/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => console.log("Database Conected"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
-}
-
-export default connectDB;
--- server/src/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-﻿const notFound = (req, res) => {
-  res.status(404).render('pages/404', {
-    layout: 'layouts/main',
-    title: '404 | Page Not Found'
-  });
-};
-
-module.exports = { notFound };
